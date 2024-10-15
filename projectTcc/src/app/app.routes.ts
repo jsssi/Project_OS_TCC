79c1d8753afc9d46637ce97c285d7b44ce0f7508
neno <jeffesilva546@gmail.com>
2024-10-15 19:56:59 -0300

    register page

--- projectTcc/src/app/app.routes.ts
+++ projectTcc/src/app/app.routes.ts
@@ -1,12 +1,17 @@
 import { Routes } from '@angular/router';
 import { CanActivateHomeGuard } from './Guard/can-deactivate.guard';
 import { LoginPageComponentComponent } from './modules/login-page-component/login-page-component.component';
+import { CadastroPageComponent } from './modules/cadastro-page/cadastro-page.component';
 
 export const routes: Routes = [
 
   {
     path: 'login',
     component: LoginPageComponentComponent
+  },
+  {
+    path:'cadastrar',
+    component:CadastroPageComponent
   },
   {
     path: '',
