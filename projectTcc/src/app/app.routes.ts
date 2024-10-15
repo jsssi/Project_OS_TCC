f2d8cfc681d7bb384e55140ecc7bd8908486ed48
Anderson Silva <andersonline62@gmail.com>
2024-10-15 09:53:15 -0300

    .

--- projectTcc/src/app/app.routes.ts
+++ projectTcc/src/app/app.routes.ts
@@ -1,22 +1,9 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './modules/login/login.component';
-import { ClientePageComponent } from './modules/cliente-page/cliente-page.component';
 import { CanActivateHomeGuard } from './Guard/can-deactivate.guard';
 
 
 export const routes: Routes = [
 
-
-  {
-    path: 'login',
-    component: LoginComponent
-  },
-  {
-
-    path: 'cliente',
-    component: ClientePageComponent,
-    canActivate: [CanActivateHomeGuard]
-  },
   {
 
     path: '',
--- projectTcc/src/app/modules/modules.module.ts
+++ projectTcc/src/app/modules/modules.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavComponent } from './nav/nav.component';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    NavComponent
   ]
 })
 export class ModulesModule { }
