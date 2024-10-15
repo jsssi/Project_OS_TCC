24139be0e740151528b6b9727141bac62b746d7b
jefferson <jeffesilva546@gmail.com>
2024-10-14 22:37:48 -0300

    main update

--- projectTcc/src/app/app.component.ts
+++ projectTcc/src/app/app.component.ts
@@ -1,15 +1,7 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
 
-<<<<<<< HEAD
-=======
-
->>>>>>> dev2_Senai
-=======
 import { NavBarComponent } from "./modules/nav-bar/nav-bar.component";
->>>>>>> 30bb6556637fb4ebc57f2eb41913b520f33633e8
-
 
 
 
@@ -17,19 +9,9 @@ import { NavBarComponent } from "./modules/nav-bar/nav-bar.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-<<<<<<< HEAD
-<<<<<<< HEAD
-  imports: [RouterOutlet,RouterLink , RouterLinkActive],
-=======
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
->>>>>>> dev2_Senai
-  template: `<router-outlet></router-outlet>`,
-=======
   imports: [RouterOutlet, RouterLink, RouterLinkActive, NavBarComponent],
-  template: `<router-outlet>
-    <app-nav-bar></app-nav-bar>
-  </router-outlet>`,
->>>>>>> 30bb6556637fb4ebc57f2eb41913b520f33633e8
+  template: `<router-outlet></router-outlet>`,
+
   styleUrl: './app.component.scss',
   encapsulation: ViewEncapsulation.None
 
