b2465b159d0945cd0d0b415e99a5fcdd9e10c7a1
jefferson <jeffesilva546@gmail.com>
2024-10-21 22:09:03 -0300

    cliente home page

---
+++ projectTcc/src/app/modules/client-page/client-page.component.ts
@@ -0,0 +1,15 @@
+import { NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-page',
+  standalone: true,
+  imports: [NgFor],
+  templateUrl: './client-page.component.html',
+  styleUrl: './client-page.component.scss'
+})
+export class ClientPageComponent {
+  users=[
+    {name:'jefferson',info:'motorola',status:'em andamento'}
+  ]
+}
