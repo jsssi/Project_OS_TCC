3d886bd7a84d844459214e304042913c0c5bee78
anderson.reis <andersonline62@gmail.com>
2024-11-29 16:29:24 -0300

    feat: added product-page and configured forms modules of angular

--- projectTcc/src/app/modules/product-page/product-page.component.ts
+++ projectTcc/src/app/modules/product-page/product-page.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component } from '@angular/core';
 import { NavBarComponent } from "../nav-bar/nav-bar.component";
+import { NgFor } from '@angular/common';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-product-page',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.scss'
 })
-export class ProductPageComponent {
+export class ProductPageComponent implements AfterViewChecked{
+   ProductForm!: FormGroup
+
+  ngAfterViewChecked(): void {
+    this.ProductForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      brand: new FormControl('', Validators.required),
+      price: new FormControl('', Validators.required),
+      integer: new FormControl('', Validators.required)
+    });
+   }
 
 }
