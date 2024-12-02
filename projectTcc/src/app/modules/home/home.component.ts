3e25a687824ca9fc7d413f36ff86784a546c77d9
anderson.reis <andersonline62@gmail.com>
2024-12-02 14:32:38 -0300

    fix: alterado o campo de clientes e produtos da home

--- projectTcc/src/app/modules/home/home.component.ts
+++ projectTcc/src/app/modules/home/home.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from "../nav-bar/nav-bar.component";
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [
+    NavBarComponent,
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
